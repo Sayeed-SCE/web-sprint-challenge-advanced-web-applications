041093dad2bc6868c9a4310640d1d617f719f910
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-06-29 12:27:56 -0700

    completed MVP challenge

---
+++ frontend/components/ProtectedRoute.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Navigate, outlet } from "react-router-dom";
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  // const { children, ...rest } = props
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (localStorage.getItem("token")) {
+          return <Component {...props} />;
+        } else {
+          return <Navigate to="/login" />;
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
